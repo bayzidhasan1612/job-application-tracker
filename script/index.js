a3b52dd5e373f7ca2e0d2cbe9f1ef3a6efe9065e
Bayzid <macbook@Bayzids-MacBook-Air.local>
2026-03-23 17:49:05 +0600

    complete the functional work

--- script/index.js
+++ script/index.js
@@ -6,9 +6,11 @@ const tabInactive = ["bg-transparent", "text-black"];
 const allJobContainer = document.getElementById("all-job-container");
 const interviewContainer = document.getElementById("interview-container");
 const rejectedContainer = document.getElementById("rejected-container");
+const emptyDashboard = document.getElementById("empty-page");
 
 function switchTab(tab) {
   const tabs = ["all", "interview", "rejected"];
+  currentTab = tab;
 
   for (const t of tabs) {
     const tabName = document.getElementById("btn-" + t);
@@ -27,21 +29,32 @@ function switchTab(tab) {
     section.classList.add("hidden");
   }
 
+  emptyDashboard.classList.add("hidden");
+
   if (tab === "all") {
     allJobContainer.classList.remove("hidden");
+    if (allJobContainer.children.length < 1) {
+      emptyDashboard.classList.remove("hidden");
+    }
   } else if (tab === "interview") {
     interviewContainer.classList.remove("hidden");
+    if (interviewContainer.children.length < 1) {
+      emptyDashboard.classList.remove("hidden");
+    }
   } else {
     rejectedContainer.classList.remove("hidden");
+    if (rejectedContainer.children.length < 1) {
+      emptyDashboard.classList.remove("hidden");
+    }
   }
+  updateJobs();
 }
 
 // jobs update
 const totalJobs = document.getElementById("total-jobs");
 const totalInterview = document.getElementById("total-interview");
 const totalReject = document.getElementById("total-reject");
-
-totalJobs.innerText = allJobContainer.children.length;
+const availableMessage = document.getElementById("available-msg");
 
 switchTab(currentTab);
 
@@ -50,18 +63,41 @@ document
   .addEventListener("click", function (event) {
     const clickedElement = event.target;
     const card = clickedElement.closest(".card");
-    const status = card.querySelector(".status");
+    const msg = card.querySelector(".msg");
     const parent = card.parentNode;
 
     if (clickedElement.classList.contains("interview")) {
+      msg.innerText = "Interviewed";
       interviewContainer.appendChild(card);
-      status.innerText = "Interviewed"
+      updateJobs();
     }
     if (clickedElement.classList.contains("rejected")) {
+      msg.innerText = "Rejected";
       rejectedContainer.appendChild(card);
-      status.innerText = "Rejected"
+      updateJobs();
     }
     if (clickedElement.classList.contains("delete")) {
-      parent.removeChild(card)
+      parent.removeChild(card);
+      updateJobs();
     }
   });
+
+function updateJobs() {
+  const counts = {
+    all: allJobContainer.children.length,
+    interview: interviewContainer.children.length,
+    rejected: rejectedContainer.children.length,
+  };
+
+  totalJobs.innerText = counts.all;
+  totalInterview.innerText = counts.interview;
+  totalReject.innerText = counts.rejected;
+
+  availableMessage.innerText = counts[currentTab];
+
+  if(counts[currentTab] < 1){
+    emptyDashboard.classList.remove("hidden");
+  }
+}
+
+updateJobs();
